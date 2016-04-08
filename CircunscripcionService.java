4581e4fc28b41a2de46294c6b2a0d1eaaf26301f
Víctor Retortillo Fernández <VictorRF.lena@gmail.com>
2016-04-08 19:36:15 +0200

    Added files via upload

---
+++ CircunscripcionService.java
@@ -0,0 +1,9 @@
+package es.uniovi.asw.business;
+
+import es.uniovi.asw.model.Circunscripcion;
+
+public interface CircunscripcionService {
+	
+	Circunscripcion findById(long idCircunscripcion);
+	
+}
