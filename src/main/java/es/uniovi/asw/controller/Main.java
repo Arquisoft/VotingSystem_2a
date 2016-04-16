834944cf56aab07783168c6e6d4f785776ebefbc
mfenix360 <dariosuarez_@hotmail.com>
2016-04-16 18:20:54 +0200

    inicio parte web

--- src/main/java/es/uniovi/asw/controller/Main.java
+++ src/main/java/es/uniovi/asw/controller/Main.java
@@ -1,24 +1,49 @@
 package es.uniovi.asw.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import es.uniovi.asw.model.Votacion;
 
 
 @Controller
 public class Main {
 
 	private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+	
+	@RequestMapping("/votaciones")
+	public ModelAndView votaciones(Model model) {
 
-	@RequestMapping("/")
-	public ModelAndView landing(Model model) {
+		LOG.info("Votaciones page access");
+		
+		List <Votacion> listaVotaciones=new ArrayList<Votacion>();
+		
+		listaVotaciones.add(new Votacion((long)1, "PP"));
+		listaVotaciones.add(new Votacion((long)2, "PSOE"));
+		listaVotaciones.add(new Votacion((long)3, "Ciudadanos"));
+		
+		//listaVotaciones= Factories.service.createVotacionService().findAll();
+		model.addAttribute("votaciones", listaVotaciones);
+		
+		return new ModelAndView("votaciones");
+		
+	}
+	
 
-		LOG.info("Landing page access");
-		return new ModelAndView("landing");
-		//return new ModelAndView("votaciones");
+	@RequestMapping("/votacion")
+	public ModelAndView votacion(Model model) {
+
+		LOG.info("Votacion page access");
+	
+		
+		return new ModelAndView("votacion");
+		
 	}
 
 }
